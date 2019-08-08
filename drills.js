7598108bb705ea515af4c20cf6ef2b60265cf556
William Wong <wwong1987@gmail.com>
2019-08-07 20:31:36 -0700

    power calculator problem completed

--- drills.js
+++ drills.js
@@ -38,4 +38,25 @@ function howManySheeps(int) {
 
 howManySheeps(5);
 
-// testing git commit credential settings
+// Power Calculator
+// Write a function called powerCalculator() that takes two parameters, an integer as a base, and another integer as an exponent. The function returns the value of the base raised to the power of the exponent. Use only exponents greater than or equal to 0 (positive numbers)
+
+// expected output: powerCalculatorRec(10,2) should return 100
+// powerCalculatorRec(10,-2) should return exponent should be >= 0
+
+function powerCalculator(base, exp) {
+    // base case
+    if (exp < 0) {
+        return 'exponent should be >= 0';
+    }
+    // general case
+    else if (exp === 0) {
+        return 1;
+    } else {
+        return base * powerCalculator(base, exp - 1);
+    }
+}
+
+console.log(powerCalculator(10, 2)); // output: 100
+console.log(powerCalculator(10, -2)); // 'exponent should be >= 0'
+
