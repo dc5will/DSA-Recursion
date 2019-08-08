e195c239a3a1d41ab9b53d4a1b2a153b3d36c757
William Wong <wypwong916@gmail.com>
2019-08-07 19:52:56 -0700

    initial commit

---
+++ drills.js
@@ -0,0 +1,13 @@
+// Use recursion to solve these problems.
+
+// What is recursion? A method of problem solving that involves a function calling itself wherein each call breaks down the problem into smaller parts until it is simplified. Recursion is like a process where you define a problem in terms of a simpler version of itself. 
+
+// recursion is NOT a data structure or an algorithm. It is a concept.
+
+// A recursive algorithm is broken down into 2 parts: A base case (stop condition) & recursive case (function calling itself). 
+
+// 1. Base case: solution to the "simplest" possible problem.
+
+// 2. Recursive case is when you use the same algorithm to solve a simpler problem of itself with the same number of parameters to solve a smaller version of the problem.
+
+
